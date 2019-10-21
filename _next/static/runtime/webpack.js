82a91d1d511ecc109fae1690e3029ecc170eb059
Alejandro Oviedo Garcia <alejandro.oviedo.g@gmail.com>
2019-10-21 20:51:37 -0300

    add touch layout

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b7aafb42f5336e5f7152";
+/******/ 	var hotCurrentHash = "c86d4f6b3d6720a005cd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
