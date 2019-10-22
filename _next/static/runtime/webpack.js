a19372890ecf617d9b9ac05085571c33722b8fc8
Alejandro Oviedo Garcia <alejandro.oviedo.g@gmail.com>
2019-10-22 12:01:41 -0300

    center color bar tooltip

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "39fde4209ca5454f2881";
+/******/ 	var hotCurrentHash = "fc3611684adba6c6a613";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
